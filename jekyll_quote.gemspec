df6087255071002979a4a9c46610ae7311378d09
Mike Slinn <mslinn@mslinn.com>
2023-01-10 12:00:07 -0500

    -

---
+++ jekyll_quote.gemspec
@@ -0,0 +1,49 @@
+# frozen_string_literal: true
+
+require_relative 'lib/jekyll_quote/version'
+
+# rubocop:disable Metrics/BlockLength
+Gem::Specification.new do |spec|
+  github = 'https://github.com/mslinn/jekyll_quote'
+
+  spec.bindir = 'exe'
+  spec.authors = ['Mike Slinn']
+  spec.email = ['mslinn@mslinn.com']
+  spec.files = Dir['.rubocop.yml', 'LICENSE.*', 'Rakefile', '{lib,spec}/**/*', '*.gemspec', '*.md']
+  spec.homepage = 'https://www.mslinn.com/blog/2020/10/03/jekyll-plugins.html#quote'
+  spec.license = 'MIT'
+  spec.metadata = {
+    'allowed_push_host' => 'https://rubygems.org',
+    'bug_tracker_uri'   => "#{github}/issues",
+    'changelog_uri'     => "#{github}/CHANGELOG.md",
+    'homepage_uri'      => spec.homepage,
+    'source_code_uri'   => github,
+  }
+  spec.name = 'jekyll_quote'
+  spec.post_install_message = <<~END_MESSAGE
+
+    Thanks for installing #{spec.name}!
+
+  END_MESSAGE
+  spec.post_install_message = <<~END_MESSAGE
+
+    Thanks for installing #{spec.name}!
+
+  END_MESSAGE
+  spec.require_paths = ['lib']
+  spec.required_ruby_version = '>= 2.6.0'
+  spec.summary = 'Provides a Jekyll filter that creates formatted quotes.'
+  spec.test_files = spec.files.grep(%r!^(test|spec|features)/!)
+  spec.version = JekyllQuoteVersion::VERSION
+
+  spec.add_dependency 'jekyll', '>= 3.5.0'
+  spec.add_dependency 'jekyll_plugin_logger'
+  spec.add_dependency 'key_value_parser'
+
+  # spec.add_development_dependency 'debase'
+  # spec.add_development_dependency 'rubocop-jekyll'
+  # spec.add_development_dependency 'rubocop-rake'
+  # spec.add_development_dependency 'rubocop-rspec'
+  # spec.add_development_dependency 'ruby-debug-ide'
+end
+# rubocop:enable Metrics/BlockLength
--- spec/jekyll_quote_spec.rb
+++ spec/jekyll_quote_spec.rb
@@ -1,21 +1,72 @@
 # frozen_string_literal: true
 
 require 'jekyll'
+require 'jekyll_plugin_logger'
 require_relative '../lib/jekyll_quote'
 
-RSpec.describe(Jekyll) do
-  include Jekyll
+Registers = Struct.new(:page, :site)
 
-  let(:config) { instance_double('Configuration') }
-  let(:context) {
-    context_ = instance_double('Liquid::Context')
-    context_.config = config
-    context_
-  }
+# Mock for Collections
+class Collections
+  def values
+    []
+  end
+end
 
-  it 'is created properly' do
-    # run_tag = RunTag.new('run', 'echo asdf')
-    # output = run_tag.render(context)
-    # expect(output).to eq('asdf')
+# Mock for Site
+class SiteMock
+  attr_reader :config
+
+  def initialize
+    @config = YAML.safe_load(File.read('_config.yml'))
+  end
+
+  def collections
+    Collections.new
+  end
+end
+
+# Mock for Liquid::ParseContent
+class TestParseContext < Liquid::ParseContext
+  attr_reader :line_number, :registers
+
+  def initialize
+    super
+    @line_number = 123
+
+    @registers = Registers.new(
+      { 'path' => 'https://feeds.soundcloud.com/users/soundcloud:users:7143896/sounds.rss' },
+      SiteMock.new
+    )
+  end
+end
+
+# Lets get this party started
+class MyTest
+  RSpec.describe Quote do
+    let(:logger) do
+      PluginMetaLogger.instance.new_logger(self, PluginMetaLogger.instance.config)
+    end
+
+    let(:parse_context) { TestParseContext.new }
+
+    let(:helper) do
+      JekyllTagHelper.new(
+        'quote',
+        "cite='This is a citation' url='https://blah.com'",
+        logger
+      )
+    end
+
+    it 'is created properly' do
+      quote = Quote.send(
+        :new,
+        'quote',
+        "cite='This is a citation' url='https://blah.com'".dup,
+        parse_context
+      )
+      result = quote.send(:render, parse_context)
+      expect(result).to eq('')
+    end
   end
 end
